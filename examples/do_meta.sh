9da1bc4f38ec11e816e6468648d2130a15e78347
dls <zhenglovejie@gmail.com>
2023-02-11 18:28:14 +0800

    更新了一些李子

--- examples/do_meta.sh
+++ examples/do_meta.sh
@@ -2,8 +2,11 @@
 
 dggridExe=../../build/src/apps/dggrid/dggrid
 # f="aigenerate"
-f="gridgenPureKML"
-fmeta="gridgenPureKML"
+f="genChildren" #文件夹所在路径
+fmeta="genchildren" #meta名字
+# fmeta="gridgenDiamond" #meta名字
+
+ 
 # fmeta="transformseq2geo"
 ##f和=之间不要有空格 sh会自动识别
 echo "#### running example out=>${fmeta}.txt" 
